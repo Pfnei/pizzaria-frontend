c9e84532352cae565a58e01587d1f9befbf56bc3
MeMySelfandPfnei <christian.pfneisl@gmail.com>
2025-10-20 19:50:08 +0200

    navbar: Visibilty of all Elements according to userrole and endpoint

--- controller/navigation.js
+++ controller/navigation.js
@@ -13,29 +13,49 @@ function navBarVisibility() {
     //Login Button,
     if (someEndpoint(["register", "login"]) || UserStorageService.isLoggedIn()) {
         $('#navLogin').hide();
+        console.log(UserStorageService.getUser())
+        console.log(UserStorageService.isAdmin())
     } else {
         $('#navLogin').show();
     }
-    //UserMenu
+    //UserMenu , Orders
     if (UserStorageService.isLoggedIn()) {
         $('#userMenu').show();
+        $('#navOrderList').show();
         const nbr = $('#navbar-right');
         nbr.removeClass("mt-3");
         nbr.addClass("mt-1");
     } else {
         $('#userMenu').hide();
+        $('#navOrderList').hide();
         const nbr = $('#navbar-right');
         nbr.removeClass("mt-1");
         nbr.addClass("mt-3");
     }
-
     //Finetuning Shoppingcart
     if (someEndpoint(["register", "login"])) {
-         const sc = $('#shoppingCart');
+        const sc = $('#navShoppingCart');
         sc.addClass("me-4");
     } else {
-        const sc = $('#shoppingCart');
+        const sc = $('#navShoppingCart');
         sc.removeClass("me-4");
+    }
+
+    //no Support Menu on Register, Login
+    if (someEndpoint(["register", "login"])) {
+        $('#navTogglerSupportContent').hide();
+   }
+
+
+    //AdminTools
+    if (UserStorageService.isLoggedIn() && UserStorageService.isAdmin()) {
+        $('#navUserList').show();
+        $('#navProductList').show();
+        $('#navOrderList').show();
+    } else {
+        $('#navUserList').hide();
+        $('#navProductList').hide();
+        $('#navOrderList').hide();
     }
 
 
