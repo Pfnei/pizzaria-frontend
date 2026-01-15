168771ddb41d76ade4064e2095edeab88f3be37a
Dany <q845999@gmail.com>
2026-01-15 11:17:15 +0100

    Refactors profile image loading
    Simplifies profile image loading by directly attempting to fetch the image and handling potential errors.
    
    Removes the initial check for profile picture existence, streamlining the process.

--- controller/userdetails.js
+++ controller/userdetails.js
@@ -95,20 +95,18 @@ async function loadUser(userId) {
 
     const profileImg = document.getElementById("profileImage");
 
-   if (user.profilePicture) {
+
   try {
     const blob = await fileService.downloadProfilePicture(userId);
     const url = URL.createObjectURL(blob);
     profileImg.src = url;
   } catch (e) {
-    console.error("Profilbild konnte nicht geladen werden:", e);
-    profileImg.src = "../assets/default-profile.png";
+    
+    // src = BACKEND + "/files/default-profile.png"; man kriegt vom backend ein bild als response zurück
   }
 
 
-} else {
-      profileImg.src = "../assets/default-profile.png";
-    }
+
     setValue("anrede", user.salutation || "");
     setValue("diversDetails", user.salutationDetail || "");
 
